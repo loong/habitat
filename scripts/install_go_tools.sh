469d69dd5ecf588a13a2c363caaa68996b5bf32a
Long Hoang <long@mindworker.de>
2017-06-09 13:58:00 +0100

    Add go tools install script

---
+++ scripts/install_go_tools.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/nsf/gocode
+go get -u github.com/rogpeppe/godef
+go get -u github.com/golang/lint/golint
+go get -u gopkg.in/alecthomas/gometalinter
+go get -u golang.org/x/tools/cmd/gorename
