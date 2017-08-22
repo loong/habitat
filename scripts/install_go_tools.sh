9497d32533392705afa7a383b23e90ba5ee4a93a
Long Hoang <long@mindworker.de>
2017-08-22 17:14:28 +0100

    Add flymake to go tools

--- scripts/install_go_tools.sh
+++ scripts/install_go_tools.sh
@@ -4,5 +4,6 @@ go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
 go get -u github.com/golang/lint/golint
+go get -u github.com/dougm/goflymake
 go get -u gopkg.in/alecthomas/gometalinter
 go get -u golang.org/x/tools/cmd/gorename
